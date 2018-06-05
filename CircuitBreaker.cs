c58eb9a4a00072e3988b7fb493fe7c6229f4b40a
Brett Robichaud <brobichaud@digimarc.com>
2018-06-05 13:53:24 -0700

    Combines circuitbreaker test

---
+++ CircuitBreaker.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+using Polly;
+using Polly.CircuitBreaker;
+using Polly.Retry;
+
+namespace PollyTester
+{
+	public class CircuitBreaker
+	{
+		private static int loop = 1;
+
+		public void TestCircuit()
+		{
+			var policy = StandardBreaker();
+			//var policy = StandardRetry();
+
+			for (loop = 1; loop <= 10; loop++)
+			{
+				try
+				{
+					if (loop == 6)
+						Thread.Sleep(5000);
+
+					Console.WriteLine($"Attempt: {loop}");
+					policy.Execute(DoSomething);
+				}
+				catch (BrokenCircuitException e)
+				{
+					Console.WriteLine($"\tCircuit broken: {e.Message}");
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine($"\t{e.Message}");
+				}
+			}
+
+			Console.WriteLine($"Complete!");
+			Console.ReadKey();
+		}
+
+		static void DoSomething()
+		{
+			if (loop > 2 && loop < 6)
+				throw new ArgumentException($"Circuit exception: {loop}");
+		}
+
+		public static Policy StandardBreaker()
+		{
+			return Policy
+				.Handle<ArgumentException>()
+				.CircuitBreaker(3, TimeSpan.FromSeconds(5));
+				//.CircuitBreaker(3, TimeSpan.FromSeconds(15), OnBreak, OnReset);
+		}
+
+		public static RetryPolicy StandardRetry()
+		{
+			return Policy.Handle<Exception>()
+				.WaitAndRetry(2, r => TimeSpan.FromMilliseconds(250), (exception, timeSpan, context) => RetryOccurred(timeSpan, exception));
+		}
+
+		private static void OnBreak(Exception arg1, TimeSpan arg2)
+		{
+			Console.WriteLine("Circuit broken");
+		}
+
+		public static void OnReset()
+		{
+			Console.WriteLine("Circuit reset");
+		}
+
+		public static void RetryOccurred(TimeSpan interval, Exception e)
+		{
+			Console.WriteLine($"Retry attempt, Waiting:{interval}\nException:\n{e}");
+		}
+	}
+}
---
+++ Retries.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Polly;
+
+namespace PollyTester
+{
+	public class Retries
+	{
+		public bool TestRetries1()
+		{
+			// handle ALL exceptions
+			var p = Policy
+				.Handle<Exception>()
+				.Retry(2, (exception, retryCount, context) =>
+				{
+					Console.WriteLine($"Exception: {exception.Message}");
+				});
+
+			short attempt = 0;
+			try
+			{
+				return p.Execute(() => GenerateError(++attempt, "this is silly"));
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"Attempt failed\nException: {e.Message}");
+			}
+
+			return false;
+		}
+
+		public bool TestRetries2()
+		{
+			// retries an operation twice on a specific exception (3 total tries)
+			var p = Policy
+				.Handle<Exception>(e => e.Message == "this is silly")
+				.WaitAndRetry(2, r => TimeSpan.FromMilliseconds(500), (exception, timeSpan, context) => LogRetry(timeSpan, exception));
+
+			short attempt = 0;
+			try
+			{
+				return p.Execute(() => GenerateError(++attempt, "this is silly"));
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"Cache availability check failed, error:\nException: {e.Message}");
+			}
+
+			return false;
+		}
+
+		public static void LogRetry(TimeSpan interval, Exception e)
+		{
+			Console.WriteLine($"Span: {interval}\nException: {e.Message}");
+		}
+
+		public static bool GenerateError(short attempt, string somedata)
+		{
+			Console.WriteLine($"Attempt #{attempt}");
+			throw new MyException(somedata);
+		}
+
+		public class MyException : ArgumentException
+		{
+			public MyException(string message) : base(message) { }
+
+			public int Value { get; set; }
+		}
+	}
+}
